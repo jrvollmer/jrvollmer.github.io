7e18d72e91b8b3058f546464038063abec42680c
James <jrvollmer@wisc.edu>
2023-02-04 18:32:36 -0600

    Added project templates

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { Link, VStack, HStack, Spacer, Text, Box, Wrap, SlideFade, Button} from '@chakra-ui/react';
-
+import { Link, VStack, HStack, Flex, Spacer, Text, Image, Box, Container, Wrap, SlideFade, Button} from '@chakra-ui/react';
+import GitHubImage from '../Assets/github.png';
+import BackgroundImage from '../Assets/background.svg';
 
 
 function Projects(props) {
@@ -15,7 +16,7 @@ function Projects(props) {
             pt='75px'
             align='center'
             spacing={0}
-            h='100vh' // TODO Might want to change to max-content if content is larger than 100vh
+            minH='100vh' // TODO Might want to change to max-content if content is larger than 100vh
         >
             <Box
                 h='0.75vh'
@@ -34,6 +35,57 @@ function Projects(props) {
             {/* TODO Move this to the location where I want to switch to being in the "Projects" section */}
             <div ref={props.prjRef}/>
 
+            <VStack pt='15px' spacing='15px'>
+                <Project
+                    image={BackgroundImage}
+                    imageH='100%'
+                    fontSize={fontSize}
+                    name='Solar Car 1 Engineering Dashboard'
+                    description='The Solar Car 1 Engineering Dashboard is a React App with a Node.js backend designed to
+                             run locally on the laptops of the engineers building Solar Car 1.'
+                    ghNames={['chase-car-dashboard']}
+                    ghLinks={['https://github.com/badgerloop-software/chase-car-dashboard']}
+                />
+                <Project
+                    image={BackgroundImage}
+                    imageH='100%'
+                    fontSize={fontSize}
+                    name='Solar Car 1 Driver Dashboard'
+                    description='Description of the driver dashboard'
+                    ghNames={['chase-car-dashboard']}
+                    ghLinks={['https://github.com/badgerloop-software/chase-car-dashboard']}
+                />
+                <Project
+                    image={BackgroundImage}
+                    imageH='100%'
+                    fontSize={fontSize}
+                    name='Battery Cell Testing Application'
+                    description='Description of the battery testing app.'
+                    ghNames={['chase-car-dashboard']}
+                    ghLinks={['https://github.com/badgerloop-software/chase-car-dashboard']}
+                />
+                <Project
+                    image={BackgroundImage}
+                    imageH='100%'
+                    fontSize={fontSize}
+                    name='Recipe Finder App'
+                    description='Description of the recipe finder app, the cloud database used, and the server used.'
+                    ghNames={['recipe-finder-app', 'recipe-finder-server']}
+                    ghLinks={['https://github.com/badgerloop-software/chase-car-dashboard','https://github.com/badgerloop-software/chase-car-dashboard']}
+                />
+                <Project
+                    image={BackgroundImage}
+                    imageH='100%'
+                    fontSize={fontSize}
+                    name='Solar Car 1 Engineering Dashboard'
+                    description='The Solar Car 1 Engineering Dashboard is a React App with a Node.js backend designed to
+                             run locally on the laptops of the engineers building Solar Car 1.'
+                    ghNames={['chase-car-dashboard']}
+                    ghLinks={['https://github.com/badgerloop-software/chase-car-dashboard']}
+                />
+            </VStack>
+
+
             {/*<Button bgColor='#808080' w='50vw' h='10vh' onClick={() => setFooBool(!fooBool)}>
 
             </Button>
@@ -46,6 +98,94 @@ function Projects(props) {
 
         </VStack>
 
+    );
+}
+
+function Project(props) {
+    return (
+        <Flex
+            w='95vw'
+            h='25vh'
+            gap='20px'
+        >
+            <Image src={props.image} fit='contain' maxH={props.imageH}/>
+            {/* TODO Remove
+            <ProjectVisual
+                image={props.image}
+                imageH={props.imageH}
+                fontSize={props.fontSize}
+            />
+            */}
+            <ProjectDescription
+                fontSize={props.fontSize}
+                name={props.name}
+                description={props.description}
+                ghNames={props.ghNames}
+                ghLinks={props.ghLinks}
+            />
+        </Flex>
+    );
+}
+
+/* TODO Remove
+function ProjectVisual(props) {
+    return (
+        <VStack
+            align='center'
+            spacing='5px'
+        >
+            <Image src={props.image} fit='contain' m={0} sx={{ maxHeight: `calc(${props.imageH} - 40px)`}} />
+
+        </VStack>
+    );
+}*/
+
+function ProjectDescription(props) {
+    return (
+        <VStack
+            flex={1}
+            bgColor='#f8f8f8'
+            borderRadius='10px'
+            pt='5px'
+            pb='20px'
+            px='20px'
+        >
+            <Text as='b' fontSize='24px' color='#202020'>
+                {props.name}
+            </Text>
+            <Text
+                w='100%'
+                textAlign='left'
+                fontSize={props.fontSize}
+                color='#404040'
+            >
+                {props.description}
+            </Text>
+            <Spacer/>
+            <Flex w='100%' gap='20px'>
+                {
+                    props.ghNames.map((val, idx) => {
+                        return (
+                            <Link href={props.ghLinks[idx]} isExternal>
+                                <HStack spacing='5px' h='35px'>
+                                    <Image src={GitHubImage} boxSize='35px'/>
+                                    <Text
+                                        fontSize={props.fontSize}
+                                        textAlign='center'
+                                        color='#24292f'
+                                        bgColor='#eff1f3'
+                                        borderRadius='7.5px'
+                                        px='10px'
+                                    >
+                                        {val}
+                                    </Text>
+                                </HStack>
+                            </Link>
+                        );
+                    })
+                }
+            </Flex>
+        </VStack>
     );
 }
 
