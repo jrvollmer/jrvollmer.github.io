7b58d67b14d3b669cf04523b3b2d7e6647f5c2f0
James <jamesvollmer02@gmail.com>
2023-09-04 01:19:09 -0500

    Skill: Clean up comments and imports

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -1,13 +1,13 @@
 import * as React from "react";
-import { Link, Card, CardBody, VStack, HStack, Spacer, Text, Box } from '@chakra-ui/react';
+import { Card, CardBody, Text } from '@chakra-ui/react';
 
 function Skill(props) {
     return (
         <Card zIndex={0}>
-            <CardBody bgColor='#f0f0f0' /* TODO Different color than the Background section background color. Decide if I want to keep this */ >
+            <CardBody bgColor='#f0f0f0' >
                 <Text
                     lineHeight={1}
-                    /* TODO */ fontSize={props.fontSize}
+                    fontSize={props.fontSize}
                     color='#404040'
                 >
                     {props.text}
